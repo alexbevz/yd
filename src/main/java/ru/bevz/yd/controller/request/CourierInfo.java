81063f040b724b5b891cea8a7761b5b2bf3b332d
Bevz Aleksandr <as-bivz@yandex.ru>
2022-01-24 01:08:11 +0300

    Edit request object: CourierInfo

--- src/main/java/ru/bevz/yd/controller/request/CourierInfo.java
+++ src/main/java/ru/bevz/yd/controller/request/CourierInfo.java
@@ -1,7 +1,6 @@
 package ru.bevz.yd.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -12,10 +11,10 @@ import java.util.List;
 public class CourierInfo {
 
     @JsonProperty("courier_id")
-    private long courierId;
+    private long id;
 
     @JsonProperty("courier_type")
-    private String typeCourier;
+    private String type;
 
     @JsonFormat(shape = JsonFormat.Shape.ARRAY)
     @JsonProperty("regions")
@@ -23,14 +22,7 @@ public class CourierInfo {
 
     @JsonFormat(shape = JsonFormat.Shape.ARRAY)
     @JsonProperty("working_hours")
+    //TODO: Will write the own valid annotation
     private List<@Pattern(regexp = "^(20|21|22|23|[01]\\d|\\d)((:[0-5]\\d){1,2})-(20|21|22|23|[01]\\d|\\d)((:[0-5]\\d){1,2})$") String> timePeriodList;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("rating")
-    private float rating;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("earnings")
-    private float earnings;
 
 }
