8c0e99e4f393686a093509b44a8cbabea59f03ae
Bevz Aleksandr <as-bivz@yandex.ru>
2022-02-05 12:17:06 +0300

    The method findTypeCourierByName was added

--- src/main/java/ru/bevz/yd/repository/TypeCourierRepository.java
+++ src/main/java/ru/bevz/yd/repository/TypeCourierRepository.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.bevz.yd.model.TypeCourier;
 
+import java.util.Optional;
+
 @Repository
 public interface TypeCourierRepository extends JpaRepository<TypeCourier, Integer> {
 
     boolean existsByName(String name);
 
     TypeCourier getTypeCourierByName(String name);
+
+    Optional<TypeCourier> findTypeCourierByName(String name);
 
 }
