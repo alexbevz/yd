d42d12e74dc55a6fb12846aeb06554b73fa69acf
Bevz Aleksandr <as-bivz@yandex.ru>
2022-02-08 22:46:02 +0300

    Deletted 2 test methods and added test method for addNewCourier

---
+++ src/test/java/ru/bevz/yd/annotation/CSVToCourierDTOForCSV.java
@@ -0,0 +1,15 @@
+package ru.bevz.yd.annotation;
+
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import ru.bevz.yd.aggregator.CourierDTOForCSVAggregator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AggregateWith(CourierDTOForCSVAggregator.class)
+public @interface CSVToCourierDTOForCSV {
+}
