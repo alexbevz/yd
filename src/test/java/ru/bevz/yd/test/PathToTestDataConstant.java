60e506dfa086e86545fb559be404c7f75a5a1165
Bevz Aleksandr <as-bivz@yandex.ru>
2022-02-13 11:33:52 +0300

    Added class with path to data

---
+++ src/test/java/ru/bevz/yd/test/PathToTestDataConstant.java
@@ -0,0 +1,26 @@
+package ru.bevz.yd.test;
+
+public class PathToTestDataConstant {
+
+    public final static String CHANGELOG_MASTER_TEST_PATH =
+            "/changelog-master-test.xml";
+
+    public final static String COURIER_PATH =
+            "/courier-data/";
+
+    public final static String COURIER_INIT_DATA_PATH =
+            COURIER_PATH + "courier-init-data.sql";
+
+    public final static String COURIER_DELETE_DATA_PATH =
+            COURIER_PATH + "courier-delete-data.sql";
+
+    public final static String COURIER_SERVICE_PATH =
+            COURIER_PATH + "service/";
+
+    public final static String COURIER_CONTROLLER_PATH =
+            COURIER_PATH + "controller/";
+
+    public final static String COURIER_REPOSITORY_PATH =
+            COURIER_PATH + "repository/";
+
+}
