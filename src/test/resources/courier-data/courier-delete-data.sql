5d6a6f57891ddc05218dadf09689369cbeb42b2d
Bevz Aleksandr <as-bivz@yandex.ru>
2022-02-18 14:07:21 +0300

    Fix testing dataset for CourierServiceTest
    The column of id was deleted when inserting init data and added resetting of sequences because there was the error EntityAlreadyExistWithId.

--- src/test/resources/courier-data/courier-delete-data.sql
+++ src/test/resources/courier-data/courier-delete-data.sql
@@ -5,3 +5,6 @@ DELETE FROM "order";
 DELETE FROM time_period;
 DELETE FROM region;
 DELETE FROM courier;
+
+ALTER SEQUENCE time_period_time_period_id_seq RESTART WITH 1;
+ALTER SEQUENCE region_region_id_seq RESTART WITH 1;
