f51895ac6c113945f3365254866406bdb1739e55
Bevz Aleksandr <as-bivz@yandex.ru>
2022-02-25 23:18:44 +0300

    Update class SecondaryService
    Added the method "getOrSaveRate". Its destiny provides the fast getting object Rate from db.
    Added the method "getOrSaveCompletedCourier". Its destiny provides the fast getting object CompletedCourier from db.

--- src/main/java/ru/bevz/yd/service/SecondaryService.java
+++ src/main/java/ru/bevz/yd/service/SecondaryService.java
@@ -3,14 +3,17 @@ package ru.bevz.yd.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.bevz.yd.model.Region;
-import ru.bevz.yd.model.TimePeriod;
+import ru.bevz.yd.model.*;
+import ru.bevz.yd.repository.CompletedCourierRepository;
+import ru.bevz.yd.repository.RateRepository;
 import ru.bevz.yd.repository.RegionRepository;
 import ru.bevz.yd.repository.TimePeriodRepository;
 import ru.bevz.yd.util.DateTimeUtils;
 
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static ru.bevz.yd.constant.GlobalConstant.GENERAL_RATE;
 
 @Service
 public class SecondaryService {
@@ -20,6 +23,12 @@ public class SecondaryService {
 
     @Autowired
     private TimePeriodRepository timePeriodRep;
+
+    @Autowired
+    private CompletedCourierRepository completedCourierRep;
+
+    @Autowired
+    private RateRepository rateRep;
 
     public Set<Region> getOrSaveRegionsByNumber(Set<Integer> numbers) {
         return numbers
@@ -46,5 +55,22 @@ public class SecondaryService {
 
         return timePeriodRep.findTimePeriodByFromAndTo(timePeriod.getFrom(), timePeriod.getTo())
                 .orElseGet(() -> timePeriodRep.save(timePeriod));
+    }
+
+    public Rate getOrSaveRate(float value) {
+        return rateRep.getRateByValue(value)
+                .orElseGet(() -> rateRep.save(new Rate().setValue(value)));
+    }
+
+    public CompletedCourier getOrSaveCompletedCourier(int courierId, int ratioId) {
+        Rate rate = getOrSaveRate(GENERAL_RATE);
+        return completedCourierRep.getCompletedCourierByOptions(courierId, ratioId, rate.getId())
+                .orElseGet(() -> completedCourierRep.save(
+                                new CompletedCourier()
+                                        .setCourier(new Courier().setId(courierId))
+                                        .setRate(rate)
+                                        .setRatio(new Ratio().setValue(ratioId))
+                        )
+                );
     }
 }
